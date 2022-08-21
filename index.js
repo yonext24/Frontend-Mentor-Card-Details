d33e905ef0d7c98804d5106270aac31db36d7fa6
Yanzz <yonielkpo@gmail.com>
2022-08-21 19:33:21 -0300

    fixed bugs with formulary

--- index.js
+++ index.js
@@ -12,13 +12,25 @@ const subButton = document.querySelector('.sub')
 
 nameInput.addEventListener('input', e => {
   const text = nameInput.value
+  if (text.length > 25) {
+    preventDefault()
+  }
   console.log(text !== '')
   if (text === '') {
     nameCard.textContent = 'Jane Appleseed'
   }
   nameCard.textContent = text
 })
+nameInput.addEventListener('keydown', (e) => {
+  const num = e.target.value
+  if(num.length > 24 && e.keyCode !== 8) {
+    e.preventDefault()
+  }
+})
 numberInput.addEventListener('input', e => {
+  if (numberInput.value.length > 16) {
+    preventDefault()
+  }
   let number = numberInput.value
   // Add the 0s to the string
   number = number.padEnd(16, '0')
@@ -33,11 +45,29 @@ numberInput.addEventListener('input', e => {
   number = number.replace(/,/g,"")
   numberCard.textContent = number
 })
+numberInput.addEventListener('keydown', (e) => {
+  const num = e.target.value
+  if(num.length > 15 && e.keyCode !== 8) {
+    e.preventDefault()
+  }
+})
 function yyaa (x, y) {
   let $ = x.value
   $ = $.padEnd(2, '0')
   y.textContent = $
 }
+yyInput.addEventListener('keydown', (e) => {
+  const num = e.target.value
+  if(num.length > 1 && e.keyCode !== 8) {
+    e.preventDefault()
+  }
+})
+mmInput.addEventListener('keydown', (e) => {
+  const num = e.target.value
+  if(num.length > 1 && e.keyCode !== 8) {
+    e.preventDefault()
+  }
+})
 
 yyInput.addEventListener('input', () => { yyaa(yyInput, yyCard) })
 mmInput.addEventListener('input', () => { yyaa(mmInput, mmCard) })
@@ -49,6 +79,12 @@ cvcInput.addEventListener('input', e => {
     cvcCard.textContent = 'Jane Appleseed'
   }
   cvcCard.textContent = text
+})
+cvcInput.addEventListener('keydown', (e) => {
+  const num = e.target.value
+  if(num.length > 2 && e.keyCode !== 8) {
+    e.preventDefault()
+  }
 })
 subButton.addEventListener('click', () => {
   subButton.preventDefault()
